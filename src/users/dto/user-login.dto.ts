59e46b41aa46d4bf177cba3b57dd30232e8bd733
Gromovoi Stanislav <gromila12@rambler.ru>
2022-11-11 16:56:21 +0600

    login

--- src/users/dto/user-login.dto.ts
+++ src/users/dto/user-login.dto.ts
@@ -1,9 +1,9 @@
-// import { IsEmail, IsString } from 'class-validator';
+import { IsEmail, IsString } from 'class-validator';
 
 export class UserLoginDto {
-	// @IsEmail({}, { message: 'Неверно указан email' })
+	@IsEmail({}, { message: 'Неверно указан email' })
 	email: string;
 
-	// @IsString()
+	@IsString()
 	password: string;
 }
